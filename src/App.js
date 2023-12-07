7708ebdd9d0a6f6f597f8824b38932911db88e41
YuriyYurev <iuriev-iurii1@mail.ru>
2023-12-07 18:24:11 +0700

    feat: counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './style/_app.scss'
+import React, { useState } from 'react';
 
 export default function App() {
+	let [counter, setCounter] = useState(0);
+
+	function increment() {
+		setCounter(counter => counter +=1 );
+	}
+
+	function dicrement() {
+		setCounter(counter => counter -= 1);
+	}
+
 	return (
 		<div className="layout">
 			<div className="container pt-5">
 				<div className="mb-5">
 					<h1 className="text-center">React app</h1>
+					<button type="button" onClick={increment}>increment</button>
+					<div>{counter}</div>
+					<button type="button" onClick={dicrement}>dicrement</button>
 				</div>
 			</div>
 		</div>
