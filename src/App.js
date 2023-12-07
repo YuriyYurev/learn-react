c0f6e04eeb9cd9619c0cbad3a19c3ad5dbcbfb42
YuriyYurev <iuriev-iurii1@mail.ru>
2023-12-07 18:34:32 +0700

    управляемый компонент состояние связано с state

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@ import './style/_app.scss'
 import React, { useState } from 'react';
 
 export default function App() {
-	let [counter, setCounter] = useState(0);
-
-	function increment() {
-		setCounter(counter => counter +=1 );
-	}
-
-	function dicrement() {
-		setCounter(counter => counter -= 1);
-	}
-
+	let [value, setValue] = useState('Текст в инпуте');
+	
 	return (
 		<div className="layout">
 			<div className="container pt-5">
 				<div className="mb-5">
 					<h1 className="text-center">React app</h1>
-					<button type="button" onClick={increment}>increment</button>
-					<div>{counter}</div>
-					<button type="button" onClick={dicrement}>dicrement</button>
+					<input type="text" value={value} onChange={event => setValue(event.target.value)} />
+					<h2>{value}</h2>
 				</div>
 			</div>
 		</div>
