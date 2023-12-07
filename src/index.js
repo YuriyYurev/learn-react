cbd631a794b5edb0f74506db60dd6301cec812fd
YuriyYurev <iuriev-iurii1@mail.ru>
2023-12-07 16:08:13 +0700

    create react-app

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+	return (
+		<div className="App">
+		<header className="App-header">
+		
+		</header>
+		</div>
+	);
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
+
