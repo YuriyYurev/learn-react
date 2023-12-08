86ee8150e1c5442a7b20c5f5980a3b0e81891a72
YuriyYurev <iuriev-iurii1@mail.ru>
2023-12-08 21:47:39 +0700

    feat: PostItem.jsx

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './style/_app.scss'
 import React, { useState } from 'react';
-import Counter from './components/Counter';
 import PostItem from './components/PostItem';
 
 export default function App() {
-	let [value, setValue] = useState('Текст в инпуте');
+	const [posts] = useState([
+		{ id: 1, title: 'Заголовок поста', description: 'Описание поста' },
+		{ id: 2, title: 'Заголовок поста 2', description: 'Описание поста 3' },
+		{ id: 3, title: 'Заголовок поста 3', description: 'Описание поста 4' },
+	])
 	
 	return (
 		<div className="layout">
 			<div className="container pt-5">
 				<div className="mb-5">
 					<h1 className="text-center">React app</h1>
-					<input type="text" value={value} onChange={event => setValue(event.target.value)} />
-					<h2>{value}</h2>
-					<Counter/>
-					<PostItem/>
+					{posts.map(post => {
+						return <PostItem post={post} key={post.id}/>
+					})}
 				</div>
 			</div>
 		</div>
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 
-export default function PostItem() {
+export default function PostItem({ post }) {
     return (
-        <div>
-            <h1>Post</h1>
+        <div className="post-item">
+            <div className="post-item__content">
+                <h3 className="post-item__content-title">{post.id} {post.title}</h3>
+                <div className="post-item__content-description">{post.description}</div>
+            </div>
+            <button type="button" className="post-item__button">Удалить пост</button>
         </div>
     )
 }
