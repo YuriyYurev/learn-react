028bc3963b87f891489051cb3e30e214dced9b57
YuriyYurev <iuriev-iurii1@mail.ru>
2023-12-08 21:57:19 +0700

    feat: PostList.jsx

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import './style/_app.scss'
 import React, { useState } from 'react';
-import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 
 export default function App() {
 	const [posts] = useState([
-		{ id: 1, title: 'Заголовок поста', description: 'Описание поста' },
-		{ id: 2, title: 'Заголовок поста 2', description: 'Описание поста 3' },
-		{ id: 3, title: 'Заголовок поста 3', description: 'Описание поста 4' },
+		{ id: 1, title: 'Пост про js', description: 'Описание поста' },
+		{ id: 2, title: 'Пост про js 2', description: 'Описание поста 3' },
+		{ id: 3, title: 'Пост про js 3', description: 'Описание поста 4' },
 	])
-	
+
 	return (
 		<div className="layout">
 			<div className="container pt-5">
 				<div className="mb-5">
 					<h1 className="text-center">React app</h1>
-					{posts.map(post => {
-						return <PostItem post={post} key={post.id}/>
-					})}
+					<PostList title={'Список постов про JS'} posts={posts}/>
 				</div>
 			</div>
 		</div>
