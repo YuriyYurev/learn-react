211570f43ea304f27c2c16c5256b9c57a8f66c22
Yuriy Yurev <yurayurev1995@gmail.com>
2023-12-12 23:35:08 +0700

    fix: cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import Drawer from "./components/Drawer";
 import Card from "./components/Card";
 
 export default function App() {
+
+	const cards = [
+		{id: '0', title: 'Кросоввки nike', price: 1298, src: '/img/sneakers/1.jpg'},
+		{id: '1', title: 'Кросоввки Adidas', price: 5398, src: '/img/sneakers/2.jpg'},
+		{id: '3', title: 'Кросоввки Puma', price: 4298, src: '/img/sneakers/3.jpg'},
+		{id: '4', title: 'Кросоввки Asics', price: 3228, src: '/img/sneakers/4.jpg'},
+	];
+
 	return (
 		<div className="wrapper clear">
 			<div className="overlay" style={{ display: 'none' }}>
@@ -18,10 +26,9 @@ export default function App() {
 					</div>
 				</div>
 				<div className="d-flex">
-					<Card/>
-					<Card/>
-					<Card/>
-					<Card/>
+					{cards.map(card => {
+						return <Card key={card.id} id={card.id} title={card.title} src={card.src} price={card.price}/>
+					})}
 				</div>
 			</div>
 		</div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
-export default function Card() {
+export default function Card({id, title, src, price}) {
     return (
-        <div className="card">
+        <div className="card" id={id}>
             <div className="favorite">
-                <img height={40} width={40} src="/img/liked.svg" alt=""/>
+                <img className="cu-p" height={40} width={40} src="/img/unliked.svg" alt=""/>
             </div>
-            <img height={133} width={112} src="/img/sneakers/1.jpg" alt=""/>
-            <p>фывфывывфы</p>
+            <img height={133} width={112} src={src} alt=""/>
+            <p>{title}</p>
             <div className="d-flex justify-between">
                 <div className="d-flex flex-column">
                     <span>Цена:</span>
-                    <b>12 999руб.</b>
+                    <b>{price}</b>
                 </div>
                 <button type="button" className="button">
                     <img height={11} width={11} src="/img/plus.svg" alt=""/>
