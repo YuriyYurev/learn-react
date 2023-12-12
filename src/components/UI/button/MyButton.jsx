43fa832cc740fd3db79786d229555de5e37ef1a5
YuriyYurev <iuriev-iurii1@mail.ru>
2023-12-12 10:54:15 +0700

    remove

--- src/components/UI/button/MyButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classess from "./MyButtons.module.css"
-
-export default function MyButton({ children, ...props }) {
-    return(
-        <button {...props} className={classess.myBtn}>{children}</button>
-    )
-}
