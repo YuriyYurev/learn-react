f3501be7ec6805fdab90e98af000ca31492147e3
Yuriy Yurev <yurayurev1995@gmail.com>
2023-12-12 22:26:38 +0700

    feat: drower

--- src/App.js
+++ src/App.js
@@ -1,6 +1,45 @@
 export default function App() {
 	return (
 		<div className="wrapper clear">
+			<div className="overlay" style={{ display: 'none' }}>
+				<div className="drawer">
+					<h2 className="d-flex justify-between mb-30">Корзина <img className="removeBtn cu-p" src="/img/btn-remove.svg" alt="Закрыть"/></h2>
+					<div className="items">
+						<div className="cardItem d-flex align-center">
+							<div style={{ backgroundImage: "url(/img/sneakers/1.jpg)", height: 70, width: 70, marginRight: 30 }} className="cartItemimg"></div>
+							<div className="mr-20">
+								<p className="mb-5">Мужские кроссовки</p>
+								<b>1025.руб.</b>
+							</div>
+							<img className="removeBtn" src="/img/btn-remove.svg" alt="Удалить"/>
+						</div>
+						<div className="cardItem d-flex align-center">
+							<div style={{ backgroundImage: "url(/img/sneakers/1.jpg)", height: 70, width: 70, marginRight: 30 }} className="cartItemimg"></div>
+							<div className="mr-20">
+								<p className="mb-5">Мужские кроссовки</p>
+								<b>1025.руб.</b>
+							</div>
+							<img className="removeBtn" src="/img/btn-remove.svg" alt="Удалить"/>
+						</div>
+					</div>
+					<div className="cartTotalBlock">
+						<ul>
+							<li className="d-flex">
+								<span>Итого:</span>
+								<div></div>
+								<b>21 498р.</b>
+							</li>
+							<li className="d-flex">
+								<span>Итого:</span>
+								<div></div>
+								<b>1074р.</b>
+							</li>
+						</ul>
+						<button type="button" className="greenButton">Оформить заказ <img className="removeBtn" src="/img/arrow.svg" alt="Оформить заказ"/></button>
+					</div>
+				</div>
+			</div>
+
 			<header className="d-flex justify-between align-center p-40">
 				<div className="d-flex align-center">
 					<img height={40} width={40} src="./img/logo.png" alt=""/>
@@ -20,11 +59,20 @@ export default function App() {
 				</ul>
 			</header> 
 			<div className="content p-40"> 
-				<h1 className="mb-40">Все кроссовки</h1>
+				<div className="d-flex align-center justify-between mb-40">
+					<h1 className="">Все кроссовки</h1>
+					<div className="search-block d-flex">
+						<img src="/img/search.svg" alt="Поиск"/>
+						<input type="text" placeholder="Поиск..."/>
+					</div>
+				</div>
 				<div className="d-flex">
 					<div className="card">
+						<div className="favorite">
+							<img height={40} width={40} src="/img/liked.svg" alt=""/>
+						</div>
 						<img height={133} width={112} src="/img/sneakers/1.jpg" alt=""/>
-						<p></p>
+						<p>фывфывывфы</p>
 						<div className="d-flex justify-between">
 							<div className="d-flex flex-column">
 								<span>Цена:</span>
