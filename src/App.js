b4d534899092b2c68ca6a606616c45d33f17ddcf
Yuriy Yurev <yurayurev1995@gmail.com>
2023-12-12 22:48:00 +0700

    feat: Drawer.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
 import Header from "./components/Header";
+import Drawer from "./components/Drawer";
 import Card from "./components/Card";
 
 export default function App() {
 	return (
 		<div className="wrapper clear">
 			<div className="overlay" style={{ display: 'none' }}>
-				<div className="drawer">
-					<h2 className="d-flex justify-between mb-30">Корзина <img className="removeBtn cu-p" src="/img/btn-remove.svg" alt="Закрыть"/></h2>
-					<div className="items">
-						<div className="cardItem d-flex align-center">
-							<div style={{ backgroundImage: "url(/img/sneakers/1.jpg)", height: 70, width: 70, marginRight: 30 }} className="cartItemimg"></div>
-							<div className="mr-20">
-								<p className="mb-5">Мужские кроссовки</p>
-								<b>1025.руб.</b>
-							</div>
-							<img className="removeBtn" src="/img/btn-remove.svg" alt="Удалить"/>
-						</div>
-						<div className="cardItem d-flex align-center">
-							<div style={{ backgroundImage: "url(/img/sneakers/1.jpg)", height: 70, width: 70, marginRight: 30 }} className="cartItemimg"></div>
-							<div className="mr-20">
-								<p className="mb-5">Мужские кроссовки</p>
-								<b>1025.руб.</b>
-							</div>
-							<img className="removeBtn" src="/img/btn-remove.svg" alt="Удалить"/>
-						</div>
-					</div>
-					<div className="cartTotalBlock">
-						<ul>
-							<li className="d-flex">
-								<span>Итого:</span>
-								<div></div>
-								<b>21 498р.</b>
-							</li>
-							<li className="d-flex">
-								<span>Итого:</span>
-								<div></div>
-								<b>1074р.</b>
-							</li>
-						</ul>
-						<button type="button" className="greenButton">Оформить заказ <img className="removeBtn" src="/img/arrow.svg" alt="Оформить заказ"/></button>
-					</div>
-				</div>
+				<Drawer/>
 			</div>
 			<Header/>
 			<div className="content p-40"> 
---
+++ src/components/Drawer.js
@@ -0,0 +1,40 @@
+export default function Drawer() {
+    return (
+        <div className="drawer">
+            <h2 className="d-flex justify-between mb-30">Корзина <img className="removeBtn cu-p" src="/img/btn-remove.svg" alt="Закрыть"/></h2>
+            <div className="items">
+                <div className="cardItem d-flex align-center">
+                    <div style={{ backgroundImage: "url(/img/sneakers/1.jpg)", height: 70, width: 70, marginRight: 30 }} className="cartItemimg"></div>
+                    <div className="mr-20">
+                        <p className="mb-5">Мужские кроссовки</p>
+                        <b>1025.руб.</b>
+                    </div>
+                    <img className="removeBtn" src="/img/btn-remove.svg" alt="Удалить"/>
+                </div>
+                <div className="cardItem d-flex align-center">
+                    <div style={{ backgroundImage: "url(/img/sneakers/1.jpg)", height: 70, width: 70, marginRight: 30 }} className="cartItemimg"></div>
+                    <div className="mr-20">
+                        <p className="mb-5">Мужские кроссовки</p>
+                        <b>1025.руб.</b>
+                    </div>
+                    <img className="removeBtn" src="/img/btn-remove.svg" alt="Удалить"/>
+                </div>
+            </div>
+            <div className="cartTotalBlock">
+                <ul>
+                    <li className="d-flex">
+                        <span>Итого:</span>
+                        <div></div>
+                        <b>21 498р.</b>
+                    </li>
+                    <li className="d-flex">
+                        <span>Итого:</span>
+                        <div></div>
+                        <b>1074р.</b>
+                    </li>
+                </ul>
+                <button type="button" className="greenButton">Оформить заказ <img className="removeBtn" src="/img/arrow.svg" alt="Оформить заказ"/></button>
+            </div>
+        </div>
+    )
+}
