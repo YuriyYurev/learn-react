47ce76eaaf4a0d1fa6635a661367359f53cabfbe
YuriyYurev <iuriev-iurii1@mail.ru>
2023-12-12 10:55:20 +0700

    remove

--- src/components/UI/input/MyInput.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classess from "./MyInput.module.css"
-
-export default function MyInput(props) {
-    return (
-        <input className={classess.myInput} {...props}/>
-    )
-}
