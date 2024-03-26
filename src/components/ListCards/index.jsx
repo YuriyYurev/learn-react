af5a7f7c032733b6ad0577145b910157a3a1783f
YuriyYurev <iuriev-iurii1@mail.ru>
2024-03-26 14:48:12 +0700

    feat: loader

--- src/components/ListCards/index.jsx
+++ src/components/ListCards/index.jsx
@@ -4,24 +4,28 @@ import Card from "../Card";
 export default function ListCards({ cards, onToggleTocart, onFavoriteToggle, searchValue }) {
     return (
         <div className={style.row}>
-            {cards
-                .filter((card) => card.title.toLowerCase().includes(searchValue.toLowerCase()))
-                .map((card) => {
-                    return (
-                        <div className={style.card} key={card.id}>
-                            <Card
-                                id={card.id}
-                                title={card.title}
-                                src={card.src}
-                                price={card.price}
-                                isCart={card.isCart}
-                                isFavorite={card.isFavorite}
-                                onToggleTocart={onToggleTocart}
-                                onFavoriteToggle={onFavoriteToggle}
-                            />
-                        </div>
-                    );
-                })}
+            {cards.length ? (
+                cards
+                    .filter((card) => card.title.toLowerCase().includes(searchValue.toLowerCase()))
+                    .map((card) => {
+                        return (
+                            <div className={style.card} key={card.id}>
+                                <Card
+                                    id={card.id}
+                                    title={card.title}
+                                    src={card.src}
+                                    price={card.price}
+                                    isCart={card.isCart}
+                                    isFavorite={card.isFavorite}
+                                    onToggleTocart={onToggleTocart}
+                                    onFavoriteToggle={onFavoriteToggle}
+                                />
+                            </div>
+                        );
+                    })
+            ) : (
+                <div className="loader"></div>
+            )}
         </div>
     );
 }
