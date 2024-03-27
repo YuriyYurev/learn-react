fc79aa6c76b01d49f51fb9fcdfebc697811190e8
Yuriy Yurev <yurayurev1995@gmail.com>
2024-03-27 16:35:58 +0700

    feat: pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+// import ListCards from "../components/ListCards";
+import React from "react";
+
+export default function App() {
+    return (
+        <>
+        <h1>Home</h1>
+            {/* <div className="d-flex align-center justify-between mb-40">
+                <h1 className="">{searchValue ? `Поиск по: ${searchValue}` : "Все кроссовки"}</h1>
+                <div className="search-block d-flex">
+                    <img src="/img/search.svg" alt="Поиск" />
+                    <input type="text" placeholder="Поиск..." value={searchValue} onChange={onChangeSearchInput} />
+                    {searchValue && (
+                        <img
+                            src="/img/btn-remove.svg"
+                            alt="Очистить поиск"
+                            className="clear cu-p"
+                            onClick={() => setSearchValue("")}
+                        />
+                    )}
+                </div>
+            </div> */}
+            {/* <ListCards
+                cards={items}
+                onToggleTocart={onToggleTocart}
+                searchValue={searchValue}
+                onFavoriteToggle={onFavoriteToggle}
+            /> */}
+        </>
+    );
+}
