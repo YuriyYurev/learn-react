cff81da4f69d0634ae18613d3a4fff8fe99926c4
Yuriy Yurev <yurayurev1995@gmail.com>
2024-03-27 16:36:45 +0700

    fix: routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
-import ListCards from "./components/ListCards";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+// import ListCards from "./components/ListCards";
 import axios from "axios";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
     const [drawerOpened, setDrawerOpened] = React.useState(false);
     const [items, setItems] = React.useState([]);
-    const [searchValue, setSearchValue] = React.useState("");
+    // const [searchValue, setSearchValue] = React.useState("");
 
-    const onToggleTocart = ({ id, changeActive }) => {
-        const obj = items.find((item) => item.id === id);
-        obj.isCart = changeActive;
-        axios.put(`https://65e73c4053d564627a8e4edd.mockapi.io/items/${id}`, obj);
-        setItems((prev) => prev.map((item) => (item.id === id ? obj : item)));
-    };
+    // const onToggleTocart = ({ id, changeActive }) => {
+    //     const obj = items.find((item) => item.id === id);
+    //     obj.isCart = changeActive;
+    //     axios.put(`https://65e73c4053d564627a8e4edd.mockapi.io/items/${id}`, obj);
+    //     setItems((prev) => prev.map((item) => (item.id === id ? obj : item)));
+    // };
 
     const onRemoveToBasket = (id) => {
         const obj = items.find((item) => item.id === id);
@@ -23,22 +26,22 @@ export default function App() {
         setItems((prev) => prev.map((item) => (item.id === id ? obj : item)));
     };
 
-    const onFavoriteToggle = ({ id, changeFavorite }) => {
-        const obj = items.find((item) => item.id === id);
-        obj.isFavorite = changeFavorite;
-        axios.put(`https://65e73c4053d564627a8e4edd.mockapi.io/items/${id}`, obj);
-        setItems((prev) => prev.map((item) => (item.id === id ? obj : item)));
-    };
+    // const onFavoriteToggle = ({ id, changeFavorite }) => {
+    //     const obj = items.find((item) => item.id === id);
+    //     obj.isFavorite = changeFavorite;
+    //     axios.put(`https://65e73c4053d564627a8e4edd.mockapi.io/items/${id}`, obj);
+    //     setItems((prev) => prev.map((item) => (item.id === id ? obj : item)));
+    // };
 
-    const onChangeSearchInput = (event) => {
-        setSearchValue(event.target.value);
-    };
+    // const onChangeSearchInput = (event) => {
+    //     setSearchValue(event.target.value);
+    // };
 
-    React.useEffect(() => {
-        axios.get("https://65e73c4053d564627a8e4edd.mockapi.io/items").then((response) => {
-            setItems(response.data);
-        });
-    }, []);
+    // React.useEffect(() => {
+    //     axios.get("https://65e73c4053d564627a8e4edd.mockapi.io/items").then((response) => {
+    //         setItems(response.data);
+    //     });
+    // }, []);
 
     return (
         <div className="wrapper clear">
@@ -51,27 +54,10 @@ export default function App() {
                 />
                 <Header onDrawerOpened={() => setDrawerOpened(true)} />
                 <div className="content p-40">
-                    <div className="d-flex align-center justify-between mb-40">
-                        <h1 className="">{searchValue ? `Поиск по: ${searchValue}` : "Все кроссовки"}</h1>
-                        <div className="search-block d-flex">
-                            <img src="/img/search.svg" alt="Поиск" />
-                            <input
-                                type="text"
-                                placeholder="Поиск..."
-                                value={searchValue}
-                                onChange={onChangeSearchInput}
-                            />
-                            {searchValue && (
-                                <img
-                                    src="/img/btn-remove.svg"
-                                    alt="Очистить поиск"
-                                    className="clear cu-p"
-                                    onClick={() => setSearchValue("")}
-                                />
-                            )}
-                        </div>
-                    </div>
-                    <ListCards cards={items} onToggleTocart={onToggleTocart} searchValue={searchValue} onFavoriteToggle={onFavoriteToggle}/>
+                    <Routes>
+                        <Route path="/" element={<Home />}></Route>
+                        <Route path="/favorites" element={<Favorites />}></Route>
+                    </Routes>
                 </div>
             </React.StrictMode>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Header({ onDrawerOpened }) {
     return (
         <header className="d-flex justify-between align-center p-40">
             <div className="d-flex align-center">
-                <img height={40} width={40} src="./img/logo.png" alt="" />
+                <Link to="/">
+                    <img height={40} width={40} src="./img/logo.png" alt="Sneakers" />
+                </Link>
                 <div className="headerInfo">
                     <h3 className="text-uppercase">React sneackers</h3>
                     <p className="opacity-5">Магазин лучших кроссовок</p>
@@ -10,11 +14,16 @@ export default function Header({ onDrawerOpened }) {
             </div>
             <ul className="d-flex">
                 <li className="mr-30 cu-p" onClick={onDrawerOpened}>
-                    <img height={18} width={18} src="./img/cart.svg" alt="" />
+                    <img height={18} width={18} src="./img/cart.svg" alt="Корзина" />
                     <span>1025.руб.</span>
                 </li>
-                <li>
-                    <img height={18} width={18} src="./img/user.svg" alt="" />
+                <li className="mr-20 cu-p">
+                    <Link to="/favorites">
+                        <img width={18} height={18} src="img/heart.svg" alt="Закладки" />
+                    </Link>
+                </li>
+                <li className="mr-20 cu-p">
+                    <img height={18} width={18} src="./img/user.svg" alt="Пользователь" />
                 </li>
             </ul>
         </header>
